c84a0cfe67c238e206a98865dbb9201ce3288e2e
Yule Ruz <ytruzdev@gmail.com>
2026-09-24 23:43:47 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig={apiKey:"AIzaSyBqNoQTy_AJDQCeAuoizwdZwkAkRfYscSc",authDomain:"crafty-print.firebaseapp.com",projectId:"crafty-print",storageBucket:"crafty-print.firebasestorage.app",messagingSenderId:"431828090055",appId:"1:431828090055:web:81bc1baac0e4f8fab812e2"};
+firebase.initializeApp(firebaseConfig);
+const db=firebase.firestore();
+emailjs.init("WRuDcUYyM9iTI2kyn");
+ 
+const ORDER_WH="https://discordapp.com/api/webhooks/1501256567662575657/L4AFsLiETkQIQVP7J05DE0Qf6CllCoIH3JmLmXIq8Iv9dgyKZmhSeJKnHF-VnwxFHNzw";
+const CS_WH="https://discordapp.com/api/webhooks/1501629808025538680/tew4UR1Ga2OvAzpp_TMgwqlP7XOGrmBTMLHlqylCzJkZkBjK3Gu_za6pWA2ReepX4VwL";
+const REVIEW_WH="https://discord.com/api/webhooks/1502733505841463418/YiR_mCGk3-ec7hpVZjfGLZFCI3pVx-npUnlmH86CSLvxzd_5vhUuK-LgGuzcTtQ1u0Wt";
+ 
